a58f07a75aae37054b8b2b4294b17dccdc7a5d87
sridhar2495 <sridharcivil2495@gmail.com>
2022-11-07 16:01:31 +0530

    1st time push

---
+++ src/test/java/org/test/Class1.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Class1 {
+	public void method1() {
+System.out.println("method 1");
+	}
+	public void method2() {
+System.out.println("method 2");
+	}
+	public void method3() {
+System.out.println("method 3");
+	}
+
+}
