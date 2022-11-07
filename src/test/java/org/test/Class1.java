28afa9bd265882026e964e492bcbb0814bc469f3
sridhar2495 <sridharcivil2495@gmail.com>
2022-11-07 17:53:19 +0530

    3 methods added

--- src/test/java/org/test/Class1.java
+++ src/test/java/org/test/Class1.java
@@ -10,5 +10,15 @@ System.out.println("method 2");
 	public void method3() {
 System.out.println("method 3");
 	}
+	public void method11() {
+		System.out.println("method 3");
+			}
+	public void method12() {
+		System.out.println("method 3");
+			}
+	public void method13() {
+		System.out.println("method 3");
+			}
+
 
 }
